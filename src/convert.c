397e49c64aa23abff761f58921191c18a3a76276
Mike Mueller <mikemueller04@hotmail.com>
2016-08-06 10:05:28 -0400

    passed convert_II_to_2

--- src/convert.c
+++ src/convert.c
@@ -1,7 +1,15 @@
+
 #include<stdio.h>
 #include "convert.h"
 
 int convert(char *romanNumeral)
 {
-    return 1;
+
+    printf("%c\n", *romanNumeral);
+    if (*romanNumeral == 'I'){
+      return 1;
+    } else {
+      return 5;
+    }
+
 }

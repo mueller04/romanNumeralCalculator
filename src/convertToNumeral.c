6e59edd566aa2414759f1f01e64dbf1781486478
Mike Mueller <mikemueller04@hotmail.com>
2016-08-18 23:11:59 -0400

    refactor convertToNumeral

--- src/convertToNumeral.c
+++ src/convertToNumeral.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include "convertToNumeral.h"
 
-static int charIndex = 0;
+int charIndex = 0;
 
 const int convertToNumeral(char* buf, int input) {
 
@@ -11,44 +11,7 @@ const int convertToNumeral(char* buf, int input) {
   }
 
   while (input > 0) {
-    if (input >= 1000) {
-      buf[charIndex] = 'M';
-      input -= 1000;
-    } else if(input >= 900) {
-      lowerNumeralChars(buf, 'C', 'M');
-      input -= 900;
-    } else if (input >= 500) {
-      buf[charIndex] = 'D';
-      input -= 500;
-    } else if (input >= 100) {
-      buf[charIndex] = 'C';
-      input -= 100;
-    } else if (input >= 90) {
-      lowerNumeralChars(buf, 'X', 'C');
-      input -= 90;
-    } else if (input >= 50) {
-      buf[charIndex] = 'L';
-      input -= 50;
-    } else if (input >= 40) {
-      lowerNumeralChars(buf, 'X', 'L');
-      input -= 40;
-    } else if (input >= 10) {
-      buf[charIndex] = 'X';
-      input -= 10;
-    } else if (input >= 9) {
-      lowerNumeralChars(buf, 'I', 'X');
-      input -= 9;
-    } else if (input >= 5) {
-      buf[charIndex] = 'V';
-      input -= 5;
-    } else if (input >= 4) {
-      lowerNumeralChars(buf, 'I', 'V');
-      input -= 4;
-    } else if (input <= 3) {
-      buf[charIndex] = 'I';
-      input -= 1;
-    }
-    charIndex++;
+      input = addCharsToBuffer(buf, input);
   }
   buf[charIndex] = '\0';
   return 0;
@@ -58,4 +21,46 @@ void lowerNumeralChars(char* buf, const char numeral1, const char numeral2) {
   buf[charIndex] = numeral1;
   charIndex++;
   buf[charIndex] = numeral2;
+}
+
+int addCharsToBuffer(char* buf, int input_remaining){
+  if (input_remaining >= 1000) {
+    buf[charIndex] = 'M';
+    input_remaining -= 1000;
+  } else if(input_remaining >= 900) {
+    lowerNumeralChars(buf, 'C', 'M');
+    input_remaining -= 900;
+  } else if (input_remaining >= 500) {
+    buf[charIndex] = 'D';
+    input_remaining -= 500;
+  } else if (input_remaining >= 100) {
+    buf[charIndex] = 'C';
+    input_remaining -= 100;
+  } else if (input_remaining >= 90) {
+    lowerNumeralChars(buf, 'X', 'C');
+    input_remaining -= 90;
+  } else if (input_remaining >= 50) {
+    buf[charIndex] = 'L';
+    input_remaining -= 50;
+  } else if (input_remaining >= 40) {
+    lowerNumeralChars(buf, 'X', 'L');
+    input_remaining -= 40;
+  } else if (input_remaining >= 10) {
+    buf[charIndex] = 'X';
+    input_remaining -= 10;
+  } else if (input_remaining >= 9) {
+    lowerNumeralChars(buf, 'I', 'X');
+    input_remaining -= 9;
+  } else if (input_remaining >= 5) {
+    buf[charIndex] = 'V';
+    input_remaining -= 5;
+  } else if (input_remaining >= 4) {
+    lowerNumeralChars(buf, 'I', 'V');
+    input_remaining -= 4;
+  } else if (input_remaining <= 3) {
+    buf[charIndex] = 'I';
+    input_remaining -= 1;
+  }
+  charIndex++;
+  return input_remaining;
 }
--- src/convertToNumeral.h
+++ src/convertToNumeral.h
@@ -2,6 +2,5 @@
 #define CONVERTTONUMERAL_H
 
 const int convertToNumeral(char* buf, int input);
-void lowerNumeralChars(char* buf, const char numeral1, const char numeral2);
 
 #endif /* CONVERTTONUMERAL_H */

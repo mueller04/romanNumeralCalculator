673c99bc255bc057f5a3bbef415da48cce6e3730
Mike Mueller <mikemueller04@hotmail.com>
2016-09-01 17:04:33 -0400

    removed duplication in addCharToBuffer and buildString

--- src/convertToNumeral.c
+++ src/convertToNumeral.c
@@ -3,6 +3,41 @@
 #include "convertToNumeral.h"
 #include <string.h>
 
+int addCharToBuffer(char* buf, int input_remaining, const int integer, const char* romanNumeral) {
+  strcat(buf, romanNumeral);
+  input_remaining -= integer;
+  return input_remaining;
+}
+
+int buildString(char* buf, int input_remaining) {
+  if (input_remaining >= 1000) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 1000, "M");
+  } else if(input_remaining >= 900) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 900, "CM");
+  } else if (input_remaining >= 500) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 500, "D");
+  } else if (input_remaining >= 100) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 100, "C");
+  } else if (input_remaining >= 90) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 90, "XC");
+  } else if (input_remaining >= 50) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 50, "L");
+  } else if (input_remaining >= 40) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 40, "XL");
+  } else if (input_remaining >= 10) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 10, "X");
+  } else if (input_remaining >= 9) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 9, "IX");
+  } else if (input_remaining >= 5) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 5, "V");
+  } else if (input_remaining >= 4) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 4, "IV");
+  } else if (input_remaining <= 3) {
+    input_remaining = addCharToBuffer(buf, input_remaining, 1, "I");
+  }
+  return input_remaining;
+}
+
 const int convertToNumeral(char* buf, int input) {
 
   if (input >= 4000 || input <= 0) {
@@ -10,53 +45,7 @@ const int convertToNumeral(char* buf, int input) {
   }
 
   while (input > 0) {
-      input = addCharsToBuffer(buf, input);
+      input = buildString(buf, input);
   }
   return 0;
-}
-
-void lowerNumeralChars(char* buf, const char* numeral1, const char* numeral2) {
-  strcat(buf, numeral1);
-  strcat(buf, numeral2);
-}
-
-int addCharsToBuffer(char* buf, int input_remaining){
-  if (input_remaining >= 1000) {
-    strcat(buf, "M");
-    input_remaining -= 1000;
-  } else if(input_remaining >= 900) {
-    lowerNumeralChars(buf, "C", "M");
-    input_remaining -= 900;
-  } else if (input_remaining >= 500) {
-    strcat(buf, "D");
-    input_remaining -= 500;
-  } else if (input_remaining >= 100) {
-    strcat(buf, "C");
-    input_remaining -= 100;
-  } else if (input_remaining >= 90) {
-    lowerNumeralChars(buf, "X", "C");
-    input_remaining -= 90;
-  } else if (input_remaining >= 50) {
-    strcat(buf, "L");
-    input_remaining -= 50;
-  } else if (input_remaining >= 40) {
-    lowerNumeralChars(buf, "X", "L");
-    input_remaining -= 40;
-  } else if (input_remaining >= 10) {
-    strcat(buf, "X");
-    input_remaining -= 10;
-  } else if (input_remaining >= 9) {
-    lowerNumeralChars(buf, "I", "X");
-    input_remaining -= 9;
-  } else if (input_remaining >= 5) {
-    strcat(buf, "V");
-    input_remaining -= 5;
-  } else if (input_remaining >= 4) {
-    lowerNumeralChars(buf, "I", "V");
-    input_remaining -= 4;
-  } else if (input_remaining <= 3) {
-    strcat(buf, "I");
-    input_remaining -= 1;
-  }
-  return input_remaining;
 }

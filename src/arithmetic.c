55f2304928e6b491b162e427656e9bca59e3dd10
Mike Mueller <mikemueller04@hotmail.com>
2016-09-01 15:24:26 -0400

    refactored check_for_null

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -3,9 +3,18 @@
 #include "convert.h"
 #include "convertToNumeral.h"
 
+int check_for_null(const char* num){
+  if (num == NULL || num[0] == '\0'){
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
 const int add(const char* num1, const char* num2, char* buf){
-  int is_null = check_for_null(num1, num2);
-  if (is_null){
+  int num1_is_null = check_for_null(num1);
+  int num2_is_null = check_for_null(num2);
+  if (num1_is_null || num2_is_null){
       return -1;
   }
 
@@ -14,19 +23,12 @@ const int add(const char* num1, const char* num2, char* buf){
 }
 
 const int subtract(const char* num1, const char* num2, char* buf){
-    int is_null = check_for_null(num1, num2, buf);
-    if (is_null){
-        return -1;
-    }
+  int num1_is_null = check_for_null(num1);
+  int num2_is_null = check_for_null(num2);
+  if (num1_is_null || num2_is_null){
+      return -1;
+  }
 
   int result = convertToNumeral(buf, convertToInt(num1) - convertToInt(num2));
   return result;
-}
-
-int check_for_null(char* num1, char* num2){
-  if (num1 == NULL || num2 == NULL || num1[0] == '\0' || num2[0] == '\0'){
-    return -1;
-  } else {
-    return 0;
-  }
 }
